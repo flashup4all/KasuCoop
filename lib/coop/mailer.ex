9cef6719cde5d54e50db41f72f7ee1274b95ef37
Bardeson Lucky <mac@macs-MacBook-Pro.local>
2020-08-16 21:32:05 +0100

    added bamboo and mailgun adapters

---
+++ lib/coop/mailer.ex
@@ -0,0 +1,6 @@
+defmodule Coop.Mailer do
+  @moduledoc """
+  Mail adapter
+  """
+  use Bamboo.Mailer, otp_app: :coop
+end
--- lib/coop_web/schema/middleware/authorize.ex
+++ lib/coop_web/schema/middleware/authorize.ex
@@ -1,18 +1,16 @@
 defmodule CoopWeb.Schema.Middleware.Authorize do
-    @behaviour Absinthe.Middleware
+  @behaviour Absinthe.Middleware
 
-    def call(resolution, role) do
-        with %{ current_user: current_user} <- resolution.context,  
-        true <- current_role?(current_user, role)
-        do
-            resolution
-        else
-            _ -> resolution |> Absinthe.Resolution.put_result({:error, "unauthorized"})
-        end
+  def call(resolution, role) do
+    with %{current_user: current_user} <- resolution.context,
+         true <- current_role?(current_user, role) do
+      resolution
+    else
+      _ -> resolution |> Absinthe.Resolution.put_result({:error, "unauthorized"})
     end
+  end
 
-    defp current_role?(%{}, :any), do: true
-    defp current_role?(%{role: role}, :role), do: true
-    defp current_role?(_, _), do: false
-    
+  defp current_role?(%{}, :any), do: true
+  defp current_role?(%{role: role}, role), do: true
+  defp current_role?(_, _), do: false
 end
